b87899e154995b8b654fac57ac8a8a48c670018c
Yriy Fedchenko <y.fedchenko@gmail.com>
2023-05-18 16:50:05 +0300

    Update fetchCountries.js

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,8 +1,9 @@
 import Notiflix from 'notiflix';
 
+const fields = 'name,capital,population,flags,languages';
+const url = 'https://restcountries.com/v3.1/name/';
 function fetchCountries(name) {
-  const fields = 'name,capital,population,flags,languages';
-  return fetch(`https://restcountries.com/v3.1/name/${name}?fields=${fields}`)
+  return fetch(`${url}${name}?fields=${fields}`)
     .then(response => {
       if (!response.ok) {
         Notiflix.Notify.failure(`Oops, there is no country with that name`);
